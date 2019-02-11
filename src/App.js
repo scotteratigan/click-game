08407bd303118e5ce37f53f13b9e85f8fb4cab46
ScottE <scotteratigan@gmail.com>
2019-02-11 13:16:33 -0800

    Score is tracked, you can now lose (end) the game.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,44 @@ import React, { Component } from 'react';
 // import './App.css';
 import Header from './components/Header';
 import GameArea from './components/GameArea';
+import GameOver from './components/GameOver';
 import Footer from './components/Footer';
 import GameTiles from './gameTiles.json';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { tiles: GameTiles };
+    this.state = { tiles: GameTiles, gameInProgress: true, score: 0 };
   }
-  clickTile = (clickedTileId) => {
-    console.log('You clicked: ' + clickedTileId);
-    this.state.tiles.forEach(tile => {
-      if (tile.id === clickedTileId) {
-        // alert('You clicked ' + tile.text);
-        tile.clicked = true;
+  clickTile = clickedTileId => {
+    const gameTiles = [...this.state.tiles];
+    console.log(gameTiles);
+    for (let i = 0; i < gameTiles.length; i++) {
+      if (gameTiles[i].id === clickedTileId) {
+        if (gameTiles[i].clicked === true) {
+          this.setState({ gameInProgress: false });
+          // alert('GAME OVER');
+        }
+        else {
+          gameTiles[i].clicked = true;
+          this.setState({ tiles: gameTiles, score: this.state.score + 1 });
+        }
       }
-    });
-    console.log(this.state.tiles);
+    }
   }
   render() {
-    console.log(this.state.tiles);
+    let mainSection;
+    if (this.state.gameInProgress) {
+      mainSection = <GameArea gameTiles={this.state.tiles} clickTile={this.clickTile} />;
+    } else {
+      mainSection = <GameOver />;
+    }
+
     return (
       <div className="App">
         <Header />
-        <GameArea gameTiles={this.state.tiles} clickTile={this.clickTile} />
+        SCORE: {this.state.score}
+        {mainSection}
         <Footer />
       </div>
     );
---
+++ src/components/GameOver.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function GameOver(props) {
+	return (
+		<main>
+			<Container className='text-center'>
+				<h1>GAME OVER MAN :/</h1>
+			</Container>
+		</main>
+	);
+}
+
+export default GameOver;
