3d729af91419f20cf59ed41c9c6f5bf14472f244
Scott Ratigan <scotteratigan@gmail.com>
2019-02-11 19:14:20 -0800

    Updates

---
+++ precache-manifest.3514cc289423aebebc41b5d3557889fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed030854747ac815aa80",
+    "url": "/static/css/main.3f49b0cc.chunk.css"
+  },
+  {
+    "revision": "ed030854747ac815aa80",
+    "url": "/static/js/main.ed030854.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3f3dff8284c6750a0508",
+    "url": "/static/js/2.3f3dff82.chunk.js"
+  },
+  {
+    "revision": "347ad24364127adb98b04ce02f21d96e",
+    "url": "/index.html"
+  }
+];
